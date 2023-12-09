30cc2397fabf8466458035ce95313d4c39b0c70e
oz0820 <85290191+oz0820@users.noreply.github.com>
2023-12-09 15:13:40 +0900

    Amazonの出品者IDの指定に対応＋大幅リファクタ

--- EC_site_short_URL_generator.js
+++ EC_site_short_URL_generator.js
@@ -1,66 +1,68 @@
 javascript:
-const referenced_site = "https://webllica.com/copy-text-to-clipboard/";
-const version = "Ver.1.2";
-const url = location.href;
-const url_split = url.split("/", 3);
-const fqdn = url_split[2];
+/*
+APP_VERSION: 2.0
+Github_Rep: https://github.com/oz0820/EC_site_short_URL_generator
+referenced_site: https://webllica.com/copy-text-to-clipboard/
+*/
+(function () {
+    const url = new URL(location.href)
+    const url_raw = location.href
+    const hostname = url.hostname
+    const path = url.pathname
 
-const isAmazon = /amazon./.test(fqdn);
-const isAliexpress = /aliexpress.com/.test(fqdn);
-const isDLsite = /dlsite.com/.test(fqdn);
-const isFanza = /dmm.co.jp/.test(fqdn);
+    const isAmazon = !!hostname.match(/amazon./)
+    const isAliexpress = !!hostname.match(/aliexpress.com/)
+    const isDLsite = !!hostname.match(/dlsite.com/)
+    const isFanza = !!hostname.match(/dmm.co.jp/)
 
-let out_url = "";
+    let out_url = ''
 
-if (isAmazon) {
-    const out_fqdn = fqdn.slice(fqdn.search(/amazon./));
-    let item_id;
-    if (url.search(/\/dp\//) !== -1) {
-        item_id = url.substr( url.search(/\/dp\//)+4, 10);
-    } else if (url.search(/\/product\//) !== -1) {
-        item_id = url.substr( url.search(/\/product\//)+9, 10);
-    } else if (url.search(/\/ASIN\//) !== -1) {
-        item_id = url.substr(url.search(/\/ASIN\//)+6, 10);
-    } else {
-        location.href = url + "&emi=AN1VRQENFRJN5";
-        exit;
+    if (isAmazon) {
+        const slim_hostname = hostname.slice(hostname.search(/amazon./))
+        let item_id
+        if (!!path.match(/\/dp\//)) {
+            item_id = path.match(/\/dp\/[A-Z0-9]+\//)[0].slice('/dp/'.length, -1)
+        } else if (!!path.match(/\/product\//)) {
+            item_id = path.match(/\/product\/[A-Z0-9]+\//)[0].slice('/product/'.length, -1)
+        }else if (!!path.match(/\/ASIN\//)) {
+            item_id = path.match(/\/ASIN\/[A-Z0-9]+\//)[0].slice('/ASIN/'.length, -1)
+        } else {
+            location.href = url + "&emi=AN1VRQENFRJN5"
+            return
+        }
+        out_url = url.protocol + '//' + slim_hostname + "/dp/" + item_id
+        if (url.searchParams.has('smid')) {
+            out_url += '?smid=' + url.searchParams.get('smid')
+        }
     }
-    out_url = "https://" + out_fqdn + "/dp/" + item_id + "/";
-}
 
-if (isAliexpress) {
-    const out_fqdn = fqdn.slice(fqdn.search(/aliexpress.com/));
-    let item_id;
-    if (url.search(/\/item\//) !== -1) {
-        item_id = url.slice(url.search(/\/item\//)+1, url.search(/.html/)+5);
-    } else {
-        alert("The URL could not be shortened.");
-        exit;
+    if (isAliexpress) {
+        if (!!url_raw.match(/\/item\//)) {
+            out_url = url.protocol + '//' + url.host + url.pathname
+        } else {
+            alert("The URL could not be shortened.")
+            return
+        }
     }
-    out_url = "https://" + out_fqdn + "/" + item_id;
-}
 
-if (isDLsite) {
-    out_url = url.split('?')[0];
-}
+    if (isDLsite) {
+        out_url = url.origin + url.pathname
+    }
 
-if (isFanza) {
-    out_url = url.split('?')[0];
-}
+    if (isFanza) {
+        out_url = url.origin + url.pathname
+    }
 
-if (!(isAmazon || isAliexpress || isDLsite || isFanza)) {
-    out_url = url;
-}
+    if (!(isAmazon || isAliexpress || isDLsite || isFanza)) {
+        out_url = url_raw
+    }
 
-let copyForm = document.createElement("textarea");
-copyForm.textContent = out_url;
-
-let bodyElm = document.getElementsByTagName("body")[0];
-bodyElm.appendChild(copyForm);
-
-copyForm.select();
-
-const retVal = document.execCommand("copy");
-bodyElm.removeChild(copyForm);
-
-exit;
+    // copy to clipboard
+    const textarea = document.createElement('textarea');
+    textarea.value = out_url;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+})
+();
